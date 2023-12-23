a4bd28ee992fb095382433833188ff31dd16d0c9
Egor Fedorov <fedorovea@intech.rshb.ru>
2023-12-23 11:48:13 +0300

    .

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ require('dotenv').config()
 
 const express = require('express')
 const sequelize = require('./db')
+const cors = require('cors')
+
 const models = require('./models/models')
 
 const PORT = process.env.PORT || 5000
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({'eblan': true})
+})
 
 const start = async () => {
     try {
