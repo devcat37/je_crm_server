c0aa50963cb6f99e1abc64270e51a3fdd056ca1f
Egor Fedorov <fedorovea@intech.rshb.ru>
2023-12-23 12:34:48 +0300

    .

--- api/v1/controllers/app_controller.js
+++ api/v1/controllers/app_controller.js
@@ -4,8 +4,8 @@ class AppController {
     async create(req, res) {
         const query = req.query
 
-        const appBundleIos = req.app_bundle_ios
-        const appBundleAndroid = req.app_bundle_android
+        const appBundleIos = query.app_bundle_ios
+        const appBundleAndroid = query.app_bundle_android
 
         if(!appBundleIos && !appBundleAndroid) {
             return ApiError.badRequest('Нет параметров app_bundle_ios или app_bundle_android!')
