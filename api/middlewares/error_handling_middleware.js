b4e9ee7f081360c93ffd85729c459d3663e9dbde
Egor Fedorov <fedorovea@intech.rshb.ru>
2023-12-23 13:53:08 +0300

    .

--- api/middlewares/error_handling_middleware.js
+++ api/middlewares/error_handling_middleware.js
@@ -8,6 +8,7 @@ module.exports = function (err, req, res, next) {
         })
     }
 
+    console.log(err)
     return res.status(500).json({
         'status': 500,
         'message': 'Internal server error',
