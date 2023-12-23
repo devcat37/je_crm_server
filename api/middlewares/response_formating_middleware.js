6426578d66214475dcf545ff40e4c6b631e9cc85
Egor Fedorov <fedorovea@intech.rshb.ru>
2023-12-23 13:22:37 +0300

    .

--- api/middlewares/response_formating_middleware.js
+++ api/middlewares/response_formating_middleware.js
@@ -1,5 +1,10 @@
 
 module.exports = function(err, req, res, next) {
+    if(err) {
+        return next(err);
+    }
+
+    // Format response.
     res.body = {
         'data': res.body,
     }
--- api/v1/controllers/app_controller.js
+++ api/v1/controllers/app_controller.js
@@ -3,24 +3,32 @@ const {App} = require('../../../models/models')
 
 class AppController {
     async create(req, res, next) {
-        const body = req.body
+        try {
+            const body = req.body
 
-        const app_bundle_ios = body.app_bundle_ios
-        const app_bundle_android = body.app_bundle_android
+            const app_bundle_ios = body.app_bundle_ios
+            const app_bundle_android = body.app_bundle_android
 
-        if(!app_bundle_ios && !app_bundle_android) {
-            return next(ApiError.badRequest('Нет параметров app_bundle_ios или app_bundle_android!'))
+            if(!app_bundle_ios && !app_bundle_android) {
+                return next(ApiError.badRequest('Нет параметров app_bundle_ios или app_bundle_android!'))
+            }
+
+            const app = await App.create({app_bundle_ios, app_bundle_android});
+            
+            res.body = app
+            return next(res)
+        } catch (error) {
+            return next(error)
         }
-
-        const app = await App.create({app_bundle_ios, app_bundle_android});
-        
-        res.body = app
-        return next(res)
     }
 
     async getAll(req, res, next) {
-        res.body = {'ping': 'pong'}
-        return next(res)
+        try {
+            res.body = {'ping': 'pong'}
+            return next(res)
+        } catch (error) {
+            return next(error)
+        }
     }
 }
 
