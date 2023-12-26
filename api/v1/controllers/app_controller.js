2cb7a40be8e02be77e39f75b1ac6af4ee8c310d3
Egor Fedorov <fedorovea@intech.rshb.ru>
2023-12-26 11:08:36 +0300

    .

--- api/v1/controllers/app_controller.js
+++ api/v1/controllers/app_controller.js
@@ -2,37 +2,7 @@ const ApiError = require('../../error/api_error')
 const { App } = require('../../../models/models')
 
 class AppController {
-    constructor () {
-        this.getByAppBundle = async (req, res, next) => {
-            try {
-                const { app_bundle_ios, app_bundle_android } = req.query
     
-                if (!(app_bundle_ios || app_bundle_android)) {
-                    return next(ApiError.badRequest('Нет параметров app_bundle_ios или app_bundle_android!'))
-                }
-    
-                let app;
-    
-                if (app_bundle_ios) {
-                    app = await App.findOne({ 
-                        where: { app_bundle_ios: app_bundle_ios },
-                        include: AppController.appAssosiations,
-                        
-                    })
-                } else if (app_bundle_android) {
-                    app = await App.findOne({ 
-                        where: { app_bundle_android: app_bundle_android },
-                        include: AppController.appAssosiations,
-                    })
-                }
-    
-                res.body = app
-                return next(res)
-            } catch (error) {
-                return next(error)
-            }
-        }
-    }
 
     static appAssosiations = [
         {
@@ -73,7 +43,7 @@ class AppController {
             const { app_bundle_ios, app_bundle_android } = req.query
 
             if (app_bundle_ios || app_bundle_android) {
-                await this.getByAppBundle(req, res, next)
+                return await this.getByAppBundle(req, res, next)
             }
 
             const apps = await App.findAll({
@@ -88,33 +58,33 @@ class AppController {
     }
 
     async getByAppBundle(req, res, next) {
-        try {
-            const { app_bundle_ios, app_bundle_android } = req.query
+        // try {
+        //     const { app_bundle_ios, app_bundle_android } = req.query
 
-            if (!(app_bundle_ios || app_bundle_android)) {
-                return next(ApiError.badRequest('Нет параметров app_bundle_ios или app_bundle_android!'))
-            }
+        //     if (!(app_bundle_ios || app_bundle_android)) {
+        //         return next(ApiError.badRequest('Нет параметров app_bundle_ios или app_bundle_android!'))
+        //     }
 
-            let app;
+        //     let app;
 
-            if (app_bundle_ios) {
-                app = await App.findOne({ 
-                    where: { app_bundle_ios: app_bundle_ios },
-                    include: AppController.appAssosiations,
+        //     if (app_bundle_ios) {
+        //         app = await App.findOne({ 
+        //             where: { app_bundle_ios: app_bundle_ios },
+        //             include: AppController.appAssosiations,
                     
-                })
-            } else if (app_bundle_android) {
-                app = await App.findOne({ 
-                    where: { app_bundle_android: app_bundle_android },
-                    include: AppController.appAssosiations,
-                })
-            }
+        //         })
+        //     } else if (app_bundle_android) {
+        //         app = await App.findOne({ 
+        //             where: { app_bundle_android: app_bundle_android },
+        //             include: AppController.appAssosiations,
+        //         })
+        //     }
 
-            res.body = app
-            return next(res)
-        } catch (error) {
-            return next(error)
-        }
+        //     res.body = app
+        //     return next(res)
+        // } catch (error) {
+        //     return next(error)
+        // }
     }
 
     async getOne(req, res, next) {
