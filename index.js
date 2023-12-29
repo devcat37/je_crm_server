f1e0d29a687b55faf434e888d38438982be8466e
Egor Fedorov <fedorovea@intech.rshb.ru>
2023-12-29 14:32:51 +0300

    .

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(errorHandler)
 const start = async () => {
     try {
         await sequelize.authenticate()
-        await sequelize.sync()
+        await sequelize.sync({ force: true })
 
         app.listen(
             PORT,
